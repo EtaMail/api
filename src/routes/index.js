98de085ab6dc1975a971413b319df5cb35c2fbaa
Alessandro Zanola <ales.zanola@gmail.com>
2022-07-20 17:34:33 +0200

    Add routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const tokenChecker = require("../middlewares/tokenChecker");
+
+const authenticationRouter = require("./authentication");
+const usersRouter = require("./users-registration");
+
 const router = express.Router();
 
 router.get('/*', function(req, res, next){
     res.setHeader('Last-Modified', (new Date()).toUTCString());
     next();
 });
+
+router.use('/v1/authenticate', authenticationRouter);
+router.use('/v1/users', usersRouter);
 
 module.exports = router;
